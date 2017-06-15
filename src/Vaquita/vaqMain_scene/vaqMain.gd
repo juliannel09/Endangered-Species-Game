00f2af379a90e790a8d7e8e5c191d7de2933c3aa
“JulianneL” <twilightsoutcast@gmail.com>
2017-06-15 16:14:08 -0700

    Make vaquita parallax background, animated vaquita avatar with up-down-side movements. Include health label.

--- src/Vaquita/vaqMain_scene/vaqMain.gd
+++ src/Vaquita/vaqMain_scene/vaqMain.gd
@@ -5,32 +5,32 @@ var returnPressed = false
 
 func _ready():
 	set_process(true)
-	Globals.set("gameRun_rh", false)
-	Globals.set("health_rh", 100)
+	Globals.set("gameRun_vaq", false)
+	Globals.set("health_vaq", 100)
 	
 # user input start/stop
 func _process(delta):
 	if Input.is_action_pressed("ui_quit"):
 		get_tree().quit()
-	if Globals.get("gameRun_rh") == true:
+	if Globals.get("gameRun_vaq") == true:
 		run_game(delta)
 	else:
 		if returnPressed:
-			get_node("healthRh_root").set_health(0)
+			get_node("healthVaq_root").set_health(0)
 		if Input.is_action_pressed("ui_accept"):
 			returnPressed = true
 			start_game()
 			
 # start game
 func start_game():
-	Globals.set("gameRun_rh", true)
-	Globals.set("health_rh", 100)
+	Globals.set("gameRun_vaq", true)
+	Globals.set("health_vaq", 100)
 	
 # Running loop
 func run_game(delta):
-	if Globals.get("health_rh")!=health:
-		health = Globals.get("health_rh")
-		get_node("healthRh_root").set_health(health)
+	if Globals.get("health_vaq")!=health:
+		health = Globals.get("health_vaq")
+		get_node("healthVaq_root").set_health(health)
 	
 	if Input.is_action_pressed("ui_cancel"):
-		Globals.set("gameRun_rh", false)
+		Globals.set("gameRun_vaq", false)
