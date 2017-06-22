1566488c8822bf32e05a00e997b3db31ae99f093
“JulianneL” <twilightsoutcast@gmail.com>
2017-06-22 16:49:06 -0700

    Jump kinda fixed...jumps correctly but also only jumps like 10 pixels. Jump bug independent of gravity and jump-force values.

--- src/Leopard/LeopPlayer_scene/kinBody_leop.gd
+++ src/Leopard/LeopPlayer_scene/kinBody_leop.gd
@@ -9,7 +9,7 @@ var canJump = false
 var jumpTimer = 0
 
 const JUMP = -700
-const GRAV = Vector2(0, 1000)
+const GRAV = 500
 const leopPadX = 200
 const leopPadY = 120
 const ACCEL = 0.5
@@ -20,7 +20,7 @@ func _ready():
 	newBack = ground.instance()
 	add_child(newBack)
 	newBack.set_owner(self)
-	get_node(".").move_to(Vector2(390, newBack.get_node("ground/ground_mech").get_pos().y - 37))
+	get_node(".").move_to(Vector2(390, newBack.get_node("ground/ground_mech").get_pos().y - 132))
 	Globals.set("gameRun_leop", true)
 	
 func _fixed_process(delta):
@@ -28,30 +28,34 @@ func _fixed_process(delta):
 		
 		jumpTimer += delta
 		
-		if((newBack.get_node("ground/ground_mech").get_pos().y - 37)==(get_node(".").get_pos().y + 100)):
+		
+		
+		if((newBack.get_node("ground/ground_mech").get_pos().y - 132)<=(get_node(".").get_pos().y)) \
+			&& (newBack.get_node("ground/ground_mech").get_pos().y - 130)>=(get_node(".").get_pos().y):
+			print("can jump")
 			jumpTimer = 0
 		
 		canJump = jumpTimer < JUMP_TIME
 		
 		if Input.is_action_pressed("ui_jump") and canJump:
+			print("is_jumping")
 			kinSpeed.y += JUMP
 			jumpTimer = JUMP_TIME
 			
-		kinSpeed += GRAV*delta
+		kinSpeed.y += GRAV*delta
 		
 		var movement = 0
 			
 		if (Input.is_action_pressed("ui_right")):
-			movement += 400
+			movement += 700
 		elif (Input.is_action_pressed("ui_left")):
-			movement -= 400
+			movement -= 700
 		else:
 			movement = 0
 			
 		
 		kinSpeed.x = lerp(kinSpeed.x, movement, ACCEL)
 		
-		print(str(kinSpeed))
 		kinSpeed = move(kinSpeed*delta)
 		
 		
