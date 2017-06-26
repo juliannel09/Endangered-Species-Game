b45a264a9d452ebcf35f6827d0db341c5c6d9c3c
“JulianneL” <twilightsoutcast@gmail.com>
2017-06-26 12:07:12 -0700

    Can finally jump and move back/forward as expected

--- src/Leopard/LeopPlayer_scene/kinBody_leop.gd
+++ src/Leopard/LeopPlayer_scene/kinBody_leop.gd
@@ -5,69 +5,71 @@ var createBack = false
 var newBack
 
 var kinSpeed = Vector2(0, 0)
+var motion = Vector2(0, 0)
 var canJump = false
 var jumpTimer = 0
 
 const JUMP = -700
-const GRAV = 500
+const GRAV = 1000
 const leopPadX = 200
 const leopPadY = 120
 const ACCEL = 0.5
 const JUMP_TIME = 0.1
+
 
 func _ready():
 	set_fixed_process(true)
 	newBack = ground.instance()
 	add_child(newBack)
 	newBack.set_owner(self)
-	get_node(".").move_to(Vector2(390, newBack.get_node("ground/ground_mech").get_pos().y - 132))
+	get_node(".").move_to(Vector2(390, newBack.get_node("ground/ground_mech").get_pos().y - 130.5))
 	Globals.set("gameRun_leop", true)
 	
 func _fixed_process(delta):
 	if Globals.get("gameRun_leop")==true:
 		
-		jumpTimer += delta
-		
-		
-		
-		if((newBack.get_node("ground/ground_mech").get_pos().y - 132)<=(get_node(".").get_pos().y)) \
-			&& (newBack.get_node("ground/ground_mech").get_pos().y - 130)>=(get_node(".").get_pos().y):
-			print("can jump")
-			jumpTimer = 0
-		
-		canJump = jumpTimer < JUMP_TIME
-		
-		if Input.is_action_pressed("ui_jump") and canJump:
-			print("is_jumping")
-			kinSpeed.y += JUMP
-			jumpTimer = JUMP_TIME
-			
-		kinSpeed.y += GRAV*delta
-		
-		var movement = 0
-			
 		if (Input.is_action_pressed("ui_right")):
-			movement += 700
+			kinSpeed.x = 700
 		elif (Input.is_action_pressed("ui_left")):
-			movement -= 700
+			kinSpeed.x = -700
 		else:
-			movement = 0
+			kinSpeed.x = 0
+		
+		
+		"""
+		var floorpos = newBack.get_node("ground/ground_mech").get_pos()
+		var leoppos = get_node(".").get_pos()
+		#print(str(leoppos.y) + " " + str(floorpos.y))
+		if(floorpos.y - 132 <=leoppos.y) && (floorpos.y - 130 >= leoppos.y):
+			jumpTimer = 0
+		"""
+		
+		
+		if is_colliding():
+			# check if the collision is with the FLOOR!
+			var collider = get_collider()
+			if Input.is_action_pressed("ui_jump") and collider==newBack.get_node("ground"):
+				print("is_jumping")
+				kinSpeed.y = JUMP
+				motion = kinSpeed*delta
+			else:
+				motion = get_collision_normal().slide(motion)
+				kinSpeed = get_collision_normal().slide(kinSpeed)
+		else:
+			kinSpeed.y += GRAV*delta
 			
+		motion = kinSpeed*delta
+		move(motion)
 		
-		kinSpeed.x = lerp(kinSpeed.x, movement, ACCEL)
-		
-		kinSpeed = move(kinSpeed*delta)
-		
-		
-		#if (get_pos().x<leopPadX):
-			#kinSpeed.x = 0
-			#move_to(Vector2(leopPadX, get_pos().y))
-		#if (get_pos().x>(get_viewport().get_rect().size.x) - 2*leopPadX):
-			#kinSpeed.x = 0
-			#move_to(Vector2(get_viewport().get_rect().size.x - 2*leopPadX, get_pos().y))
-		#if (get_pos().y<leopPadY):
-			#kinSpeed.y = 0
-			#move_to(Vector2(get_pos().x, leopPadY))
+		if (get_pos().x<leopPadX):
+			kinSpeed.x = 0
+			move_to(Vector2(leopPadX, get_pos().y))
+		if (get_pos().x>(get_viewport().get_rect().size.x) - 2*leopPadX):
+			kinSpeed.x = 0
+			move_to(Vector2(get_viewport().get_rect().size.x - 2*leopPadX, get_pos().y))
+		if (get_pos().y<leopPadY):
+			kinSpeed.y = 0
+			move_to(Vector2(get_pos().x, leopPadY))
 			
 		#if get_node(".").is_colliding():
 			#Globals.set("health_leop", Globals.get("health_leop") + get_collider().points)
